2205c4ab86ee6efbd307f76bf1ee86defe124551
joseph reis <josephreis646@gmail.com>
2023-02-14 22:05:09 -0500

    Added questions and created them as const

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+// Define the quiz questions and answers
+const quizQuestions = [
+  {
+    question: "What does HTML stand for?",
+    choices: ["Hyper Text Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language", "None of the above"],
+    answer: "Hyper Text Markup Language"
+  },
+  {
+    question: "What does CSS stand for?",
+    choices: [ "Colorful Style Sheets", "Creative Style Sheets", "Computer Style Sheets", "Cascading Style Sheets",],
+    answer: "Cascading Style Sheets"
+  },
+  {
+    question: "What does JS stand for?",
+    choices: [ "JavaSuper", "JavaScript", "JustScript", "None of the above"],
+    answer: "JavaScript"
+  }
+];
+
